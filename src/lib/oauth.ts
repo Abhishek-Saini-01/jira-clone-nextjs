7cd4fbe1630864daaa5d76eb0613c7804df09c58
Abhishek Saini <abhisheksaini655@gmail.com>
2024-12-05 00:09:27 +0530

    revert some changes

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -6,24 +6,27 @@ import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
     const { account } = await createAdminClient();
-    // const origin = headers().get("origin");
+    //@ts-expect-error @typescript-eslint/ban-ts-comment
+    const origin = headers().get("origin");
 
     const redirectUrl = await account.createOAuth2Token(
         OAuthProvider.Github,
-        `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-        `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+        `${origin}/oauth`,
+        `${origin}/sign-up`,
     );
 
     return redirect(redirectUrl);
 };
 export async function signUpWithGoogle() {
     const { account } = await createAdminClient();
-    // const origin = headers().get("origin");
+    //@ts-expect-error @typescript-eslint/ban-ts-comment
+    const origin = headers().get("origin");
+
 
     const redirectUrl = await account.createOAuth2Token(
         OAuthProvider.Google,
-        `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
-        `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
+        `${origin}/oauth`,
+        `${origin}/sign-up`,
     );
 
     return redirect(redirectUrl);
